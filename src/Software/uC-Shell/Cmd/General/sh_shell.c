bc43544696cd6a4c1c9d0c7f4621c1a26106d1fc
WHJWNAVY <navy@mercku.com>
2021-05-25 22:13:15 +0800

    bugfix for Sh_help

--- src/Software/uC-Shell/Cmd/General/sh_shell.c
+++ src/Software/uC-Shell/Cmd/General/sh_shell.c
@@ -103,9 +103,9 @@ static  CPU_INT16S  ShShell_version(CPU_INT16U        argc,
 
 static  SHELL_CMD  ShShell_CmdTbl [] =
 {
-    {"Sh_help",    ShShell_help},
+    {"Sh_help",    ShShell_help   },
     {"Sh_version", ShShell_version},
-    {0,         0           }
+    {0,         0                 }
 };
 
 
@@ -182,7 +182,7 @@ static  CPU_INT16S  ShShell_help (CPU_INT16U        argc,
                                   SHELL_OUT_FNCT    out_fnct,
                                   SHELL_CMD_PARAM  *pcmd_param)
 {
-    CPU_CHAR           cmd_str[SHELL_CFG_MODULE_CMD_NAME_LEN_MAX + 4];
+    CPU_CHAR           cmd_str[TERMINAL_CFG_MAX_CMD_LEN];
     SHELL_ERR          err;
     SHELL_CMD         *pcmd;
     SHELL_MODULE_CMD  *pmodule_cmd;
@@ -282,7 +282,8 @@ static const sh_version_t SH_VERSIONS[] = {
 };
 
 #define SH_OUT_FMT_DELIMETER " : "
-#define SH_OUT_FMT_VERSIONS "Components : Versions\r\n-------------------------"
+#define SH_OUT_FMT_VERSIONS "Components : Versions"
+#define SH_OUT_LINE_DELIMETER "--------------------"
 
 static  CPU_INT16S  ShShell_version (CPU_INT16U        argc,
                                   CPU_CHAR         *argv[],
@@ -292,32 +293,36 @@ static  CPU_INT16S  ShShell_version (CPU_INT16U        argc,
     CPU_INT16U         veri = 0;
     sh_version_t      *vert = NULL;
 
+    if ((argc != 1) && (argc != 2)) {
+        goto cmd_err;
+    }
+
     if (argc == 2) {
         if (Str_Cmp(argv[1], SH_SHELL_STR_HELP) == 0) { // -h
             (void)out_fnct(SH_SHELL_ARG_ERR_VERSION, (CPU_INT16U)Str_Len(SH_SHELL_ARG_ERR_VERSION), pcmd_param->pout_opt);
             (void)out_fnct(SH_SHELL_NEW_LINE,        (CPU_INT16U)Str_Len(SH_SHELL_NEW_LINE),        pcmd_param->pout_opt);
             (void)out_fnct(SH_SHELL_CMD_EXP_VERSION, (CPU_INT16U)Str_Len(SH_SHELL_CMD_EXP_VERSION), pcmd_param->pout_opt);
             (void)out_fnct(SH_SHELL_NEW_LINE,        (CPU_INT16U)Str_Len(SH_SHELL_NEW_LINE),        pcmd_param->pout_opt);
-            return (SHELL_ERR_NONE);
+        } else {
+            goto cmd_err;
+        }
+    } else {
+        (void)out_fnct(SH_OUT_FMT_VERSIONS, (CPU_INT16U)Str_Len(SH_OUT_FMT_VERSIONS), pcmd_param->pout_opt);
+        (void)out_fnct(SH_SHELL_NEW_LINE,   (CPU_INT16U)Str_Len(SH_SHELL_NEW_LINE),   pcmd_param->pout_opt);
+        (void)out_fnct(SH_OUT_LINE_DELIMETER, (CPU_INT16U)Str_Len(SH_OUT_LINE_DELIMETER), pcmd_param->pout_opt);
+        (void)out_fnct(SH_SHELL_NEW_LINE,   (CPU_INT16U)Str_Len(SH_SHELL_NEW_LINE),   pcmd_param->pout_opt);
+
+        for(veri=0; veri<(sizeof(SH_VERSIONS)/sizeof(sh_version_t)); veri++) {
+            vert = &(SH_VERSIONS[veri]);
+            (void)out_fnct(vert->component,       (CPU_INT16U)Str_Len(vert->component),       pcmd_param->pout_opt);
+            (void)out_fnct(SH_OUT_FMT_DELIMETER,  (CPU_INT16U)Str_Len(SH_OUT_FMT_DELIMETER),  pcmd_param->pout_opt);
+            (void)out_fnct(vert->version,         (CPU_INT16U)Str_Len(vert->version),         pcmd_param->pout_opt);
+            (void)out_fnct(SH_SHELL_NEW_LINE,     (CPU_INT16U)Str_Len(SH_SHELL_NEW_LINE),     pcmd_param->pout_opt);
         }
     }
-
-    if ((argc != 1) && (argc != 2)) {
-        (void)out_fnct(SH_SHELL_ARG_ERR_VERSION, (CPU_INT16U)Str_Len(SH_SHELL_ARG_ERR_VERSION), pcmd_param->pout_opt);
-        (void)out_fnct(SH_SHELL_NEW_LINE,        (CPU_INT16U)Str_Len(SH_SHELL_NEW_LINE),        pcmd_param->pout_opt);
-        return (SHELL_EXEC_ERR);
-    }
-
-    (void)out_fnct(SH_OUT_FMT_VERSIONS, (CPU_INT16U)Str_Len(SH_OUT_FMT_VERSIONS), pcmd_param->pout_opt);
-    (void)out_fnct(SH_SHELL_NEW_LINE,   (CPU_INT16U)Str_Len(SH_SHELL_NEW_LINE),   pcmd_param->pout_opt);
-
-    for(veri=0; veri<(sizeof(SH_VERSIONS)/sizeof(sh_version_t)); veri++) {
-        vert = &(SH_VERSIONS[veri]);
-        (void)out_fnct(vert->component,       (CPU_INT16U)Str_Len(vert->component),       pcmd_param->pout_opt);
-        (void)out_fnct(SH_OUT_FMT_DELIMETER,  (CPU_INT16U)Str_Len(SH_OUT_FMT_DELIMETER),  pcmd_param->pout_opt);
-        (void)out_fnct(vert->version,         (CPU_INT16U)Str_Len(vert->version),         pcmd_param->pout_opt);
-        (void)out_fnct(SH_SHELL_NEW_LINE,     (CPU_INT16U)Str_Len(SH_SHELL_NEW_LINE),     pcmd_param->pout_opt);
-    }
-
     return (SHELL_ERR_NONE);
+cmd_err:
+    (void)out_fnct(SH_SHELL_ARG_ERR_VERSION, (CPU_INT16U)Str_Len(SH_SHELL_ARG_ERR_VERSION), pcmd_param->pout_opt);
+    (void)out_fnct(SH_SHELL_NEW_LINE,        (CPU_INT16U)Str_Len(SH_SHELL_NEW_LINE),        pcmd_param->pout_opt);
+    return (SHELL_EXEC_ERR);
 }
